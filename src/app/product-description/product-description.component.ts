a687dd58d9e3433204caddd855754b34dd9726ac
Iannah Imoter Paul <imoteriannah@gmail.com>
2020-10-01 08:50:43 +0100

    Update product-description.component.ts

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-poduct-description',
   templateUrl: './poduct-description.component.html',
   styleUrls: ['./poduct-description.component.css']
 })
-export class PoductDescriptionComponent implements OnInit {
-
-  constructor() { }
-
+export class ProductDescriptionComponent implements OnInit {
+	albumInfo: any;
+  constructor(private _productService: ProductService) { }
   ngOnInit() {
+   this._productService.getAlbum(1)
+          .subscribe(response => this.albumInfo = response);
   }
 
 }
